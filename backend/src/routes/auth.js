1eeaa258bc43f1e3c900ad11f801453516234d7f
Mayank Saini <mayanksaini0416@gmail.com>
2025-12-05 14:31:51 +0530

    Update auth.js

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, refreshToken } = require('../controllers/authController');
+const auth = require('../middlewares/auth');
+const { register, login, refreshToken,getMe } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/refresh', refreshToken);
+//protected route
+router.get('/me', auth, getMe)
 
 module.exports = router;
