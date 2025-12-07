d6b7a4a14ca99f08d74b7dd6c66eea72f31ac86a
Mayank Saini <mayanksaini0416@gmail.com>
2025-12-07 19:26:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect } from 'react';
 import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import CreateHabit from './pages/CreateHabit';
+import Welcome from './pages/welcomePage/welcome'
+import Login from './pages/loginPage/Login';
+import Register from './pages/signupPage/Register';
+import Dashboard from './pages/dashboardPage/Dashboard';
+import CreateHabit from './pages/createHabitPage/CreateHabit';
 import { AuthContext } from './contexts/AuthContext';
 
 export default function App() {
@@ -12,15 +13,16 @@ export default function App() {
 
   useEffect(() => {
     // If not logged in, go to login
-    if (!user) navigate('/login');
+    if (!user) navigate('/Welcome');
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <Routes>
+      <Route path="/welcome" element={<Welcome />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+      <Route path="/" element={user ? <Dashboard /> : <Navigate to="/Welcome" />} />
        <Route path="/createhabit" element={<CreateHabit />} />
     </Routes>
   );
