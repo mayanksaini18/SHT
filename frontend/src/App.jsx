eec032bf40d1f9cf64d35ad366e16a47db55518c
Mayank Saini <mayanksaini0416@gmail.com>
2025-12-07 20:08:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useContext, useEffect } from 'react';
 import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
-import Welcome from './pages/welcomePage/welcome';
 import Login from './pages/loginPage/Login';
 import Register from './pages/registerPage/Register';
 import Dashboard from './pages/dashboardPage/Dashboard';
 import CreateHabit from './pages/createHabitPage/CreateHabit';
+import Welcome from './pages/welcomePage/welcome';
 import { AuthContext } from './contexts/AuthContext';
 
 export default function App() {
