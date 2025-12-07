81e1ef419b03d7a4b7d58f72ac94ba62441ccd99
Mayank Saini <mayanksaini0416@gmail.com>
2025-12-07 20:03:46 +0530

    welcone name update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
-import Welcome from './pages/welcomePage/Welcome';
+import Welcome from './pages/welcomePage/welcome';
 import Login from './pages/loginPage/Login';
 import Register from './pages/registerPage/Register';
 import Dashboard from './pages/dashboardPage/Dashboard';
