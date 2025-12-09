afa6f0852487b1c101bbd8d6381c5d71a59a8338
Mayank Saini <mayanksaini0416@gmail.com>
2025-12-09 14:40:33 +0530

    including firebase

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bg: '#f6fbff',
+        card: '#ffffff',
+        muted: '#61708a',
+        primary: '#4f59e1',
+        accent: '#ffd570',
+      }
+    },
+  },
+  plugins: [],
+}
