1a09d2da40b8949ba488d00b9ef8f3daf470e1f0
Mayank Saini <mayanksaini0416@gmail.com>
2026-01-02 15:04:27 +0530

    weeklyAnalytics route

--- backend/src/routes/habits.js
+++ backend/src/routes/habits.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/auth');
-const { createHabit, getHabits, checkIn } = require('../controllers/habitController');
+const { createHabit, getHabits, checkIn , weeklyAnalytics } = require('../controllers/habitController');
 
 router.use(auth);
 router.post('/', createHabit);
 router.get('/', getHabits);
 router.post('/:id/checkin', checkIn);
+router.get('/analytics/weekly',weeklyAnalytics)
 
 module.exports = router;
