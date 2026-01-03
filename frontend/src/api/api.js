2927fb54cdb4faed5eb62857f5409d3bf2bb164d
Mayank Saini <mayanksaini0416@gmail.com>
2026-01-03 17:39:10 +0530

    some baseURL related changes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:  'https://sht-0yzn.onrender.com/api'
-  // baseURL : 'http://localhost:5000/api'
+  // baseURL:  'https://sht-0yzn.onrender.com/api'
+  baseURL : 'http://localhost:5000/api'
  ,
     headers: {
     "Content-Type": "application/json",
